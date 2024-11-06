6f57e69a023d8911193d5cab33b14015cececec2
LubnaAjnal <lubna_ajnal@secab.org>
2024-11-06 17:55:55 +0530

    created api route

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,4 +1,3 @@
-
 <!DOCTYPE html>
 <html lang="en">
 
@@ -23,7 +22,7 @@
                     </div>
                     <div class="row clearfix">
                         <div class="">
-                            <form>
+                            <form >
                                 <div class="input_field"> <span><i aria-hidden="true" class="fa fa-user"></i></span>
                                     <input type="text" name="fullname" placeholder="Fullname" required />
                                 </div>
@@ -46,12 +45,15 @@
 
                                 <div class="row clearfix">
                                     <div class="col_half">
-                                        <div class="input_field"> <span><i aria-hidden="true" class="fa fa-calendar"></i></span>
-                                            <input type="date" name="arrival_date" placeholder="Arrival Date" required />
+                                        <div class="input_field"> <span><i aria-hidden="true"
+                                                    class="fa fa-calendar"></i></span>
+                                            <input type="date" name="arrival_date" placeholder="Arrival Date"
+                                                required />
                                         </div>
                                     </div>
                                     <div class="col_half">
-                                        <div class="input_field"> <span><i aria-hidden="true" class="fa fa-clock"></i></span>
+                                        <div class="input_field"> <span><i aria-hidden="true"
+                                                    class="fa fa-clock"></i></span>
                                             <input type="time" name="arrival_time" placeholder="Arrival Time" />
                                         </div>
                                     </div>
@@ -62,35 +64,90 @@
                                     <input type="radio" name="radiogroup1" id="rd2">
                                     <label for="rd2">Female</label>
                                 </div> --}}
+
+                                <div class="input_field"> <span><i aria-hidden="true"
+                                            class="fa fa-location-arrow"></i></span>
+                                    <input type="text" name="working_place" placeholder="Working Place" required />
+                                </div>
+                                <!-- End Working Place Field -->
+
+                                <div class="input_field"> <span><i aria-hidden="true"
+                                            class="fa fa-location-arrow"></i></span>
+                                    <input type="text" name="stay_selected_at" placeholder="Staying Place"
+                                        required />
+                                </div>
+                                <!-- End Stay Selected at  Field -->
+
+                                <div class="input_field"> <span><i aria-hidden="true"
+                                            class="fa fa-location-arrow"></i></span>
+                                    <input type="text" name="departuring_place" placeholder="Departuring Place"
+                                        required />
+                                </div>
+                                <!-- End Departuring Place  Field -->
+
+                                <div class="row clearfix">
+                                    <div class="col_half">
+                                        <div class="input_field"> <span><i aria-hidden="true"
+                                                    class="fa fa-calendar"></i></span>
+                                            <input type="date" name="departuring_date" placeholder="Departuring Date"
+                                                required />
+                                        </div>
+                                    </div>
+                                    <div class="col_half">
+                                        <div class="input_field"> <span><i aria-hidden="true"
+                                                    class="fa fa-clock"></i></span>
+                                            <input type="time" name="departuring_time"
+                                                placeholder="Departuring Time" />
+                                        </div>
+                                    </div>
+                                </div>
+
+                                <div class="row clearfix">
+                                    <div class="col_half">
+                                        <div class="input_field select_option">
+                                            <select>
+                                                <option>Select adults</option>
+                                                <option>Option 1</option>
+                                                <option>Option 2</option>
+                                                <option>Option 3</option>
+                                                <option>Option 4</option>
+                                            </select>
+                                            <div class="select_arrow"></div>
+                                        </div>
+                                    </div>
+                                    <div class="col_half">
+                                        <div class="input_field select_option">
+                                            <select>
+                                                <option>Select children</option>
+                                                <option>Option 1</option>
+                                                <option>Option 2</option>
+                                                <option>Option 3</option>
+                                                <option>Option 4</option>
+                                            </select>
+                                            <div class="select_arrow"></div>
+                                        </div>
+                                    </div>
+                                </div>
+
                                 <div class="input_field select_option">
                                     <select>
-                                        <option>Select a country</option>
-                                        <option>Option 1</option>
-                                        <option>Option 2</option>
+                                        <option>Select accomodation system request</option>
+                                        <option value="yes">Yes</option>
+                                        <option value="no">No</option>
                                     </select>
                                     <div class="select_arrow"></div>
                                 </div>
-                                <div class="input_field checkbox_option">
-                                    <input type="checkbox" id="cb1">
-                                    <label for="cb1">I agree with terms and conditions</label>
-                                </div>
-                                <div class="input_field checkbox_option">
-                                    <input type="checkbox" id="cb2">
-                                    <label for="cb2">I want to receive the newsletter</label>
-                                </div>
+
                                 <input class="button" type="submit" value="Register" />
                             </form>
                         </div>
                     </div>
                 </div>
             </div>
-            <p class="credit">Developed by <a href="http://www.designtheway.com" target="_blank">Medya Web Tech</a></p>
+            <p class="credit">Developed by <a href="http://www.designtheway.com" target="_blank">Medya Web Tech</a>
+            </p>
 
         </div>
     </div>
-
-
-
 </body>
-
 </html>
